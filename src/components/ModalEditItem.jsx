001f067948ea0b14a53cdd764918a0cba5092efb
mojtaba nabilo <mojjtaba00@gmail.com>
2023-06-25 16:53:53 +0430

    add ModalEditItem Component

--- src/components/ModalEditItem.jsx
+++ src/components/ModalEditItem.jsx
@@ -1,39 +1,116 @@
-import React, {useState} from 'react';
+import { useState, useContext } from 'react';
+import styled from "styled-components";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { dataProvider } from '../App';
 
-const ModalEditItem = () => {
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+// styled-components
+const StyledInputs = styled.div`
+    width: 100%;
+    div{
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        margin-top: 10px;
+        label{
+            margin-left: 10px;
+        }
+        .inpt-txt{
+            border-radius: 3px;
+            padding: 0 10px;
+            border: solid #c9c9c9 2px;
+            &:focus{
+                outline: none;
+            }
+        }
 
-    return (
-        <div
-            className="modal show"
-            style={{ display: 'block', position: 'absolute', direction: "rtl", zIndex: "100"}}
-        >
+    }
+    .upload-image{
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        img{
+            width: 50px;
+            height: 50px;
+        }
+        i{
+            font-size: 25px;
+            cursor: pointer;
+            margin-right: 10px;
+        }
+        .upload-btn{
+            width: fit-content;
+            border-radius: 5px;
+            border: solid 2px #c9c9c9;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding-right: 10px;
+        }
+    }
+`;
 
-            <Modal
-                show={show}
-                onHide={handleClose}
-                backdrop="static"
-                keyboard={false}
-            >
-                <Modal.Header closeButton>
-                <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                I will not close if you click outside me. Don not even try to press
-                escape key.
-                </Modal.Body>
-                <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>بستن</Button>
-                <Button variant="primary">اعمال</Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    );
-};
+function ModalEditItem() {
+  const objItem = useContext(dataProvider);
+  const {newItem, setNewItem, item, setItem, setShowModal, edit, setEdit} = objItem;
+const [image, setImage] = useState(null);
+
+  return (
+    <>
+      <Modal
+        show={edit}
+        onHide={() => setEdit(false)}
+        backdrop="static"
+        keyboard={false}
+        style={{direction: "rtl"}}
+      >
+        <Modal.Header>
+          <Modal.Title>لطفا مقادیر را تغییر دهید</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <StyledInputs>
+            <div>
+                <label>نام آیتم:</label>
+                <input name="name" type="text" className="inpt-txt" onChange={e => setNewItem({...newItem, [e.target.name] : e.target.value})}/>
+            </div>
+            <div>
+                <label>قیمت:</label>
+                <input name="price" type="text" className="inpt-txt" onChange={e => setNewItem({...newItem, [e.target.name] : e.target.value})}/>
+            </div>
+            <div className="upload-image">
+                <label>عکس:</label>
+                {
+                    image ? <div className="image-remove">
+                        <img src={image} alt="food-image"/>
+                        <i className="bi bi-trash" onClick={() => setImage(null)}></i>
+                    </div> : <div className="upload-btn">
+                        <label htmlFor="input-file" style={{cursor: "pointer"}}>
+                            بارگذاری عکس
+                        </label>
+                        <input 
+                            type="file"
+                            id="input-file" 
+                            style={{display: "none"}}
+                            name="image"
+                            onChange={e => {
+                                setImage(URL.createObjectURL(e.target.files[0]));
+                                setNewItem({...newItem, [e.target.name] : e.target.value});
+                            }}
+                        />
+                    </div>
+                }
+            </div>
+          </StyledInputs>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setEdit(false)}>
+            بستن
+          </Button>
+          <Button variant="primary">اعمال</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
 
 export default ModalEditItem;
